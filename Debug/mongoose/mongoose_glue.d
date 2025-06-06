3454aa6fbbc3df74f76d744f0bfdaac7e3e5d83a
Electrical 1 <Electrical@vtr.com>
2025-06-06 08:57:18 -0400

    First commit

---
+++ Debug/mongoose/mongoose_glue.d
@@ -0,0 +1,6 @@
+mongoose/mongoose_glue.o: ../mongoose/mongoose_glue.c \
+ ../mongoose/mongoose_glue.h ../mongoose/mongoose.h \
+ ../mongoose/mongoose_config.h
+../mongoose/mongoose_glue.h:
+../mongoose/mongoose.h:
+../mongoose/mongoose_config.h:
